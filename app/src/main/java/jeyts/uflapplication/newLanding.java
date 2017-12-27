eb584afde820b6d9f1428f742261bef6d5ba4311
Unknown <jamestylergarrison@gmail.com>
2017-12-27 16:46:56 -0600

    newlanding

---
+++ app/src/main/java/jeyts/uflapplication/newLanding.java
@@ -0,0 +1,13 @@
+package jeyts.uflapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class newLanding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_landing);
+    }
+}
